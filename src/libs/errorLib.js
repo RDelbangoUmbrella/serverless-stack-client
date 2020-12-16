f3debcee908400dbc38d553054a3308a339f9bbb
RDelbangoUmbrella <74671205+RDelbangoUmbrella@users.noreply.github.com>
2020-12-16 13:41:25 -0500

    Adding error reporting

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,10 +1,51 @@
-export function onError(error) {
-    let message = error.toString();
-  
-    // Auth errors
-    if (!(error instanceof Error) && error.message) {
-      message = error.message;
-    }
-  
-    alert(message);
+import * as Sentry from "@sentry/browser";
+
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+  if (isLocal) {
+    return;
   }
+
+  Sentry.init({
+    dsn: "https://dc05d14a15804d0886da7f03b306ca77@o474197.ingest.sentry.io/5560029",
+    autoSessionTracking: true,
+    integrations: [
+      new Integrations.BrowserTracing(),
+    ],
+  
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });  
+}
+
+export function logError(error, errorInfo = null) {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.withScope((scope) => {
+    errorInfo && scope.setExtras(errorInfo);
+    Sentry.captureException(error);
+  });
+}
+
+export function onError(error) {
+  let errorInfo = {};
+  let message = error.toString();
+
+  // Auth errors
+  if (!(error instanceof Error) && error.message) {
+    errorInfo = error;
+    message = error.message;
+    error = new Error(message);
+    // API errors
+  } else if (error.config && error.config.url) {
+    errorInfo.url = error.config.url;
+  }
+
+  logError(error, errorInfo);
+
+  alert(message);
+}
